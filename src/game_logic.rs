20af1c76864428a1d3f4b727dba2d0db21d1331c
hi <hi@pop-pc.lan>
2024-03-19 20:41:45 -0700

    worked on input system

--- src/game_logic.rs
+++ src/game_logic.rs
@@ -12,3 +12,18 @@ pub fn dislay_board(board : &[char]) {
     }
     println!();
 }
+
+pub fn get_input(board : &[char]) -> usize {
+    loop {
+        let mut input_str = String::new();
+        std::io::stdin().read_line(&mut input_str).expect("error: unable to read user input");
+        let input: usize = input_str.trim().parse().expect("Invalid input");
+        let out_of_range: bool = input > 8;
+        // TODO: panics on letter inputs
+        if out_of_range || !(board[input].is_digit(10)) {
+            println!("Invalid Input, try again");
+        } else {
+            return input;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,16 @@ fn main() {
     let mut board : [char; 9] = ['0', '1', '2',
                                  '3', '4', '5',
                                  '6', '7', '8'];
-    dislay_board(&board);
+    let mut turn = 0;
+    loop {
+        dislay_board(&board);
+        if turn % 2 == 0 {
+            // player 1 move
+            board[get_input(&board)] = 'X';
+        } else {
+            // player 2 move
+            board[get_input(&board)] = 'O';
+        }
+        turn += 1;
+    }
 }
