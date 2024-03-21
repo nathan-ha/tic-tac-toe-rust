eb8f65c5bcfe4155c6df039bf3af95703954eb42
Nathan Ha <nathan_ha@outlook.com>
2024-03-21 15:39:43 -0700

    finished game

--- src/game_logic.rs
+++ src/game_logic.rs
@@ -1,5 +1,5 @@
 // displays the board in terminal
-pub fn dislay_board(board : &[char]) {
+pub fn dislay_board(board: &[char]) {
     for i in 0..3 {
         print!(" {} ", board[i]);
     }
@@ -16,23 +16,56 @@ pub fn dislay_board(board : &[char]) {
 
 // gets user input for the position on the board, and validates input
 // returns the input
-pub fn get_input(board : &[char]) -> usize {
+pub fn get_input(board: &[char]) -> usize {
     loop {
         let mut input_str = String::new();
-        std::io::stdin().read_line(&mut input_str).expect("error: unable to read user input");
+        std::io::stdin()
+            .read_line(&mut input_str)
+            .expect("error: unable to read user input");
         match input_str.trim().parse::<usize>() {
             Ok(input) => {
                 if input <= 8 && board[input].is_digit(10) {
                     return input;
                 }
             }
-            Err(_) => {},
+            Err(_) => {}
         }
         println!("Invalid Input, try again")
     }
 }
 
 // returns 'x' or 'o' for the respective winner. 't' for tie.
-pub fn check_winner(board : &[char]) -> char {
-
+pub fn check_winner(board: &[char]) -> char {
+    // 8 checks
+    if board[0] == board[1] && board[1] == board[2] {
+        return board[0];
+    }
+    if board[3] == board[4] && board[4] == board[5] {
+        return board[3];
+    }
+    if board[6] == board[7] && board[7] == board[8] {
+        return board[6];
+    }
+    if board[0] == board[3] && board[3] == board[6] {
+        return board[0];
+    }
+    if board[1] == board[4] && board[4] == board[7] {
+        return board[1];
+    }
+    if board[2] == board[5] && board[5] == board[8] {
+        return board[2];
+    }
+    if board[0] == board[4] && board[4] == board[8] {
+        return board[0];
+    }
+    if board[2] == board[4] && board[4] == board[6] {
+        return board[2];
+    }
+    // check for tie
+    for i in 0..9 {
+        if board[i].is_digit(10) {
+            return 'n'; // if there is a digit left, then there is no tie
+        }
+    }
+    return 't';
 }
--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,29 @@ fn main() {
                                  '3', '4', '5',
                                  '6', '7', '8'];
     let mut turn = 0;
+    dislay_board(&board);
     loop {
         println!("--------------------------------");
-        dislay_board(&board);
         if turn % 2 == 0 {
             // player 1 move
             println!("Player 1 move");
-            board[get_input(&board)] = 'X';
+            board[get_input(&board)] = 'x';
         } else {
             // player 2 move
             println!("Player 2 move");
-            board[get_input(&board)] = 'O';
+            board[get_input(&board)] = 'o';
         }
         println!("--------------------------------");
+        dislay_board(&board);
+        let winner: char = check_winner(&board);
+        if winner == 't' {
+            println!("Tie!");
+            return;
+        }
+        if winner == 'x' || winner == 'o' {
+            println!("{} wins!", winner);
+            return;
+        }
         turn += 1;
     }
 }
